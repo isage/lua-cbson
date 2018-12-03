62924b393a216c72764a058e378e74132e8c60db
Ivan Shalganov <a.cobest@gmail.com>
2018-12-03 15:30:56 +0300

    The check_cbson_binary function should not be called in cbson_binary_new. Otherwise, the "Invalid operand" error occurs"

--- src/cbson-binary.h
+++ src/cbson-binary.h
@@ -15,12 +15,11 @@ typedef struct {
   char* data;
 } cbson_binary_t;
 
-int cbson_binary_create(lua_State* L, uint8_t type, const char* binary, unsigned int size);
+cbson_binary_t* cbson_binary_create(lua_State* L, uint8_t type, const char* binary, unsigned int size);
 int cbson_binary_new(lua_State* L);
 cbson_binary_t* check_cbson_binary(lua_State *L, int index);
 
 extern const struct luaL_Reg cbson_binary_meta[];
-
 extern const struct luaL_Reg cbson_binary_methods[];
 
 #endif
