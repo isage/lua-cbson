69145203212dbca70b6ce822f2d36f3bf91a6363
ufownl <ufownl@gmail.com>
2019-06-29 12:21:25 +0800

    Make the constructor of cbson.oid accept cbson.oid or string

--- src/cbson-oid.c
+++ src/cbson-oid.c
@@ -5,6 +5,7 @@
 
 #include "cbson.h"
 #include "cbson-oid.h"
+#include "cbson-util.h"
 
 DEFINE_CHECK(OID, oid)
 
@@ -27,9 +28,20 @@ int cbson_oid_create(lua_State* L, const char* oid)
 
 int cbson_oid_new(lua_State* L)
 {
-  const char* oid = luaL_checkstring(L, 1);
-
-  return cbson_oid_create(L, oid);
+  if (lua_isuserdata(L, 1) && luaL_checkudata_ex(L, 1, OID_METATABLE))
+  {
+    return cbson_oid_create(L, ((cbson_oid_t*)lua_touserdata(L, 1))->oid);
+  }
+  else if (lua_isstring(L, 1))
+  {
+    return cbson_oid_create(L, lua_tostring(L, 1));
+  }
+  else
+  {
+    lua_pushstring(L, "Invalid operand. Expected 'cbson.oid' or 'string'");
+    lua_error(L);
+    return 0;
+  }
 }
 
 int cbson_oid_tostring(lua_State* L)
